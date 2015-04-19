68f34322d857b9fde7622ac72ab64188150ce8ba
Stan <you@example.com>
2015-04-19 23:06:12 +0200

    INter Stan

--- PrPong/src/GamePong/Players/IA.java
+++ PrPong/src/GamePong/Players/IA.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 
 public class IA {
 	private EvolutiveIA evo;
-	
-	public IA(){
+
+	public IA() {
 		evo = new EvolutiveIA(6, 3);
 	}
-public int getNext(int dir, int bolaX, int bolaY, int stickX, int stickY){
-	int out = 0;
-	ArrayList<Double> inputs = new ArrayList<Double>();
-	inputs.add((double) (dir));
-	inputs.add((double) bolaX);
-	inputs.add((double) bolaY);
-	inputs.add((double) stickX);
-	inputs.add((double) stickY);
-	ArrayList<Double> output = evo.process(inputs);
-	int max = 0;
-	for(int i = 1; i < output.size(); i++){
-		if(output.get(i) > output.get(max))
-			max = i;
+
+	public int getNext(int bolaX, int bolaY, int stickX, int stickY) {
+		int out = 0;
+		ArrayList<Double> inputs = new ArrayList<Double>();
+		inputs.add((double) bolaX);
+		inputs.add((double) bolaY);
+		inputs.add((double) stickX);
+		inputs.add((double) stickY);
+		ArrayList<Double> output = evo.process(inputs);
+		int max = 0;
+		for (int i = 1; i < output.size(); i++) {
+			if (output.get(i) > output.get(max))
+				max = i;
+		}
+		return max;
 	}
-	return max;
-}
 }
--- PrPong/src/GamePong/Structures/Ball.java
+++ PrPong/src/GamePong/Structures/Ball.java
@@ -5,18 +5,45 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public class Ball {
-
 	private Rectangle ballArea;
 	
 	private int ballWidth = 20;
 	private int ballHeight = 20;
-	
+	private double Xdir = 0;
+	private double Ydir = 0;
+	private double xPos = 0.0;
+	private double yPos = 0.0;
+
 	public Ball(int widthScreen, int heightScreen) {
-		ballArea = new Rectangle(widthScreen/2 - ballWidth/2, heightScreen/2 - ballHeight/2, ballWidth, ballHeight);
+		Xdir = widthScreen / 2 - ballWidth / 2;
+		Ydir = heightScreen / 2 - ballHeight / 2;				
+		ballArea = new Rectangle((int) Xdir, (int) Ydir, ballWidth, ballHeight);
 	}
 	
-	public void refresh() {
+	public choque(int where){
+		switch (where) {
+		case 0:
+				
+			break;
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		default:
+			break;
+	}
 		
+	}
+
+	public void refresh() {
+		xPos += Xdir;
+		yPos += Ydir;
+		ballArea.setLocation((int) xPos, (int) yPos);
 	}
 	
 	public void render(Graphics g) {
