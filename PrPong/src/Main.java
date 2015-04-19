18cbd16eff20cd02d590c9aae5995ef910fbb862
Andrei <Andrei@192.168.1.90>
2015-04-19 21:16:24 +0200

    Main y jframe

--- PrPong/src/Main.java
+++ PrPong/src/Main.java
@@ -1,4 +1,11 @@
+import Screen.Screen;
+
 
 public class Main {
 
+	public static void main(String[] args) {
+		Screen screen = new Screen();
+		
+		screen.start();
+	}
 }
--- PrPong/src/Screen/Screen.java
+++ PrPong/src/Screen/Screen.java
@@ -1,15 +1,29 @@
 package Screen;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 import GamePong.Game;
 
-public class Screen implements Runnable {
+public class Screen extends Canvas implements Runnable {
 	
-	private int width, height;
+	private int width = 900;
+	private int height = width / 16 * 9;
+	private String title = "Pong";
 	private Teclado teclado;
 	private long lastTime;
 	private Game game;
 	private boolean running;
 	
+	private JFrame frame;
+	
+	public void Screen() {
+		Dimension dimension = new Dimension(width, height);
+		frame = new JFrame();
+		setMinimumSize(dimension);
+	}
 	
 	public void render() {
 		
@@ -26,5 +40,15 @@ public class Screen implements Runnable {
 	
 	public void fpsSetter() {
 		
+	}
+	
+	public void start() {
+		frame.setResizable(false);
+		frame.setTitle(title);
+		frame.add(this);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 }
